267db9435c6dd9e86c0fa7e50e78c73f84290582
ppmclala <mattymcmanus@gmail.com>
2025-12-05 14:25:36 -0500

    aoc 3

--- 3/Escalator.java
+++ 3/Escalator.java
@@ -1,21 +1,28 @@
 int maxDigit(String s) {
-    IO.println("[%s]".formatted(s));
     return s.chars().max().orElseThrow();
 }
 
 long calculateJoltage(String bank) {
     int maxPosition = Integer.MIN_VALUE;
-    char max = Character.MIN_VALUE;
+    char max = Character.MIN_VALUE, prevMax = Character.MIN_VALUE;
     for (int i = 0; i < bank.length(); ++i) {
         char batteryLabel = bank.charAt(i);
         if (batteryLabel > max) {
+            prevMax = max;
             maxPosition = i;
             max = batteryLabel;
         }
     }
 
-    int secondMax = maxDigit(bank.substring(maxPosition + 1));
+    int secondMax;
+    if (maxPosition == bank.length() - 1) {
+        secondMax = max;
+        max = prevMax;
+    } else {
+        secondMax = maxDigit(bank.substring(maxPosition + 1));
+    }
 
+    IO.println("%d:%d:%d".formatted(max-48, secondMax, (max-48) * 10 + (secondMax-48)));
     return (max-48) * 10 + (secondMax-48);
 }
 
