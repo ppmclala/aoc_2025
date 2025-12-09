cb4922e7426bd8f91824e494dfe3991785e70793
ppmclala <mattymcmanus@gmail.com>
2025-12-09 09:18:08 -0500

    day 3 part 2

--- 3/Escalator.java
+++ 3/Escalator.java
@@ -1,30 +1,39 @@
-long calculateJoltage(String bank) {
+record Battery(int pos, char label) {}
+
+List<Battery> selectBatteries(String bank, int start, int guard, List<Battery> batteries) {
     int maxPosition = Integer.MIN_VALUE;
-    char max = Character.MIN_VALUE, prevMax = Character.MIN_VALUE;
-    for (int i = 0; i < bank.length(); ++i) {
-        char batteryLabel = bank.charAt(i);
-        if (batteryLabel > max) {
-            prevMax = max;
+    char max = Character.MIN_VALUE;
+    for (int i = start; i < bank.length() - (guard-1); ++i) {
+        char label = bank.charAt(i);
+        if (label > max) {
             maxPosition = i;
-            max = batteryLabel;
+            max = label;
         }
     }
 
-    int secondMax;
-    if (maxPosition == bank.length() - 1) {
-        secondMax = max;
-        max = prevMax;
-    } else {
-        secondMax = bank.substring(maxPosition + 1).chars().max().orElseThrow();
-    }
+    batteries.add(new Battery(maxPosition, max));
 
-    return (max-48) * 10 + (secondMax-48);
+    if (--guard > 0) {
+        return selectBatteries(bank, maxPosition + 1, guard, batteries);
+    } else {
+        return batteries;
+    }
 }
 
-void main() throws IOException {
-    var totalJoltage = Files.readAllLines(Path.of("input.txt"))
+long calcJoltage(List<Battery> batteries) {
+    String selectedBatteries = batteries
         .stream()
-        .map(l -> calculateJoltage(l))
+        .map(b -> String.valueOf(b.label()))
+        .collect(Collectors.joining(""));
+    return Long.parseLong(selectedBatteries);
+}
+
+void main(String[] args) throws IOException {
+    var numBatteries = Integer.parseInt(args[1]);
+    var totalJoltage = Files.readAllLines(Path.of(args[0]))
+        .stream()
+        .map(l -> selectBatteries(l, 0, numBatteries, new ArrayList<>()))
+        .map(this::calcJoltage)
         .reduce((sum, j) -> sum + j).orElseThrow();
 
     IO.println("Total Joltage: %d".formatted(totalJoltage));
