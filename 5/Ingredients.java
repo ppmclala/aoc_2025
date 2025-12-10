c34d18bc9cd3908a480bfacdacddb693b1ee4016
ppmclala <mattymcmanus@gmail.com>
2025-12-10 08:12:56 -0500

    refactor final solution for readability

--- 5/Ingredients.java
+++ 5/Ingredients.java
@@ -2,6 +2,7 @@ record Range(long start, long end) {}
 
 List<Range> ranges = new ArrayList<>();
 Set<Long> freshIngredients = new HashSet<>();
+List<Range> allFreshIngredients = new ArrayList<>();
 
 void parseRange(String line) {
     String[] range = line.split("-");
@@ -16,9 +17,37 @@ void sortRanges() {
     ranges.sort((r1, r2) -> Long.compare(r1.start(), r2.start()));
 }
 
-void main(String[] args) throws IOException {
+void tallyAllFreshIngredients() {
+    Range nextRange = new Range(ranges.get(0).start(), ranges.get(0).end());
+
+    for (int i=1; i < ranges.size(); ++i) {
+        Range curr = ranges.get(i);
+        if (nextRange.end() < curr.start()) {
+            allFreshIngredients.add(nextRange);
+            // is there a record clone pattern?
+            nextRange = new Range(curr.start(), curr.end());
+        } else if (curr.start() >= nextRange.start() && curr.end() <= nextRange.end()) {
+            continue;
+        } else if (curr.start() >= nextRange.start() && curr.end() > nextRange.end()) {
+            nextRange = new Range(nextRange.start(), curr.end());
+        }
+    }
+
+    allFreshIngredients.add(nextRange);
+}
+
+void validateAllFreshIngredients() {
+    for (int i=1; i < allFreshIngredients.size(); ++i) {
+        Range prev = allFreshIngredients.get(i-1), curr = allFreshIngredients.get(i);
+        assert prev.end() < curr.start()  : "misaligned: prev:%s <-> curr:%s".formatted(prev, curr);
+        assert prev.start() <= prev.end() : "invalid ranage: %s".formatted(prev);
+        assert curr.start() <= curr.end() : "invalid ranage: %s".formatted(curr);
+    }
+}
+
+void parseDatabase(Path p) throws NumberFormatException, IOException {
     boolean parsingRanges = true;
-    for (String line : Files.readAllLines(Path.of(args[0]))) {
+    for (String line : Files.readAllLines(p)) {
         if (line.isBlank()) {
             parsingRanges = false;
             continue;
@@ -30,52 +59,22 @@ void main(String[] args) throws IOException {
             if (inAnyRange(ingredient)) freshIngredients.add(ingredient);
         }
     }
+}
+
+long countAllFreshIngredients() {
+    return allFreshIngredients.stream().mapToLong(r -> (r.end() - r.start()) + 1).sum();
+}
+
+void main(String[] args) throws IOException {
+    parseDatabase(Path.of(args[0]));
 
     IO.println("freshIngredients: %d".formatted(freshIngredients.size()));
 
     sortRanges();
 
-    // agg fresh in separate list
-    // start with first -> nextRange
-    // scan:
-    //      case disjoint -> collect nextRange; start new nextRange
-    //      case enclosed -> skip
-    //      case overlap  -> update nextRange.end
+    tallyAllFreshIngredients();
 
-    List<Range> freshIngredients = new ArrayList<>();
-    Range nextRange = new Range(ranges.get(0).start(), ranges.get(0).end());
+    validateAllFreshIngredients();
 
-    for (int i=1; i < ranges.size(); ++i) {
-        Range curr = ranges.get(i);
-
-        if (nextRange.end() < curr.start()) {
-            IO.println("DISJOINT: (%s-%s)".formatted(curr, nextRange));
-            freshIngredients.add(nextRange);
-            // is there a record clone pattern?
-            nextRange = new Range(curr.start(), curr.end());
-        } else if (curr.start() >= nextRange.start() && curr.end() <= nextRange.end()) {
-            IO.println("ENCLOSED: (%s-%s)".formatted(curr, nextRange));
-            continue;
-        } else if (curr.start() >= nextRange.start() && curr.end() > nextRange.end()) {
-            IO.println("OVERLAP: (%s-%s)".formatted(curr, nextRange));
-            nextRange = new Range(nextRange.start(), curr.end());
-        }
-    }
-
-    freshIngredients.add(nextRange);
-
-    for (int i=1; i < freshIngredients.size(); ++i) {
-        Range prev = freshIngredients.get(i-1), curr = freshIngredients.get(i);
-        assert prev.end() < curr.start()  : "misaligned: prev:%s <-> curr:%s".formatted(prev, curr);
-        assert prev.start() <= prev.end() : "invalid ranage: %s".formatted(prev);
-        assert curr.start() <= curr.end() : "invalid ranage: %s".formatted(curr);
-    }
-
-    var ingredientCount = freshIngredients
-        .stream()
-        .mapToLong(r -> (r.end() - r.start()) + 1)
-        .sum();
-
-    IO.println("freshIngredients: %s".formatted(freshIngredients));
-    IO.println("allPossibleIngredients: %d".formatted(ingredientCount));
+    IO.println("allPossibleIngredients: %d".formatted(countAllFreshIngredients()));
 }
